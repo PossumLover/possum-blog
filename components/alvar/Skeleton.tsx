267e2a26d942999fcf48f8979aa753dd618e3e85
PossumLover <jhswarner@gmail.com>
2023-09-19 19:25:11 -0700

    Revert "t"
    This reverts commit 0a6fb7ad68780702a8571d77561735af8678d6cd.

--- components/alvar/Skeleton.tsx
+++
@@ -1,3 +0,0 @@
-export function Skeleton({ className }: { className: string }) {
-  return <div className={`bg-skeleton motion-safe:animate-pulse rounded ${className}`} />
-}
--- components/alvar/SkeletonText.tsx
+++
@@ -1,5 +0,0 @@
-import { Skeleton } from './Skeleton'
-
-export function SkeletonText({ className }: { className: string }) {
-  return <Skeleton className={`rounded-lg py-2.5 ${className}`} />
-}
